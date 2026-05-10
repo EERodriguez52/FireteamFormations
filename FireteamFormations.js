99ef0cecfdad9cd7bf22ab0c1a1c7b72b73f9cb9
Emilio Rodriguez <emilioerodriguez@lewisu.edu>
2026-05-10 18:01:57 -0500

    all

--- FireteamFormations.js
+++ FireteamFormations.js
@@ -1,6 +1,24 @@
 const backyard = document.getElementById("backyard");
+
 const columnButton = document.getElementById("column-btn");
+
 const wedgeButton = document.getElementById("wedge-btn");
+
 const skirmisherButton = document.getElementById("skirmisher-btn");
+
 const echelonButton = document.getElementById("echelon-btn");
 
+const formations = {
+    column: [
+        {role: "Team Leader", x: 50, y: 15},
+        {role: "Automatic Rifleman", x: 50, y: 35},
+        {role: "Grenadier", x:50, y: 55},
+        {role: "Rifleman", x: 50, y: 75}
+    ] 
+};
+
+function renderFormation(column) {
+
+}
+
+console.log("JavaScript connected succesfully");
