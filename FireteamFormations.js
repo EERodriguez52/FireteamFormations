1504d069e953771f8d81d4a58b7ed1fc06c6fc3b
Emilio Rodriguez <emilioerodriguez@lewisu.edu>
2026-05-10 17:03:44 -0500

    updated all files

---
+++ FireteamFormations.js
@@ -0,0 +1,6 @@
+const backyard = document.getElementById("backyard");
+const columnButton = document.getElementById("column-btn");
+const wedgeButton = document.getElementById("wedge-btn");
+const skirmisherButton = document.getElementById("skirmisher-btn");
+const echelonButton = document.getElementById("echelon-btn");
+
